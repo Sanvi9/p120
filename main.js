9934f89ca51f4d5d4a5f6b9a26fe5b7fe7a04854
Sanvi9 <74234358+Sanvi9@users.noreply.github.com>
2021-08-14 16:09:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    console.log("p5 has called setup function");
+    classifier = ml5.imageClassifier('MobileNet',modelLoaded)
+  }
+  
 console.log("ml5.version = ",ml5.version); 
 
 classifier = ml5.imageClassifier("MobileNet",modelLoaded);
@@ -20,3 +29,28 @@ classifier = ml5.imageClassifier("MobileNet",modelLoaded);
          document.getElementById("object_name").innerHTML = results[0].label;
      }
  }
+
+  
+  function modelLoaded() {
+    console.log("modelLoaded")
+  }
+  function draw() {
+    console.log("p5 has called draw function");
+    image(video,0,0,300,300)
+    classifier.classify(video,gotResult);
+  }
+  
+  function gotResult(error,results) {
+   if (error) {
+     console.log(error)
+   } else {
+     console.log(results);
+     document.getElementById("result_oject_name").innerHTML = results[0].label;
+     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+   }
+  }
+  
+  
+  
+  
+  
